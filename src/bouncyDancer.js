0e083910abbe681339a12e80cb09cdc668fb9355
dmeidlin <git_hub@nym.hush.com>
2015-12-01 22:54:26 -0500

    Refactor the Dancer and BlinkyDancer classes into the pseudo-classical instantiation pattern and create two new buttons on dancefloor.html for creating two new sub-classes of dancers.

---
+++ src/bouncyDancer.js
@@ -0,0 +1,24 @@
+var BouncyDancer = function(top, left, timeBetweenSteps) {
+  Dancer.call(this, top, left, timeBetweenSteps);
+  this.$node = $('<span class="bouncyDancer"></span>');
+  this.setPosition(top,left);
+};
+
+BouncyDancer.prototype = Object.create(Dancer.prototype);
+BouncyDancer.prototype.constructor = BouncyDancer;
+
+BouncyDancer.prototype.step = function() {
+  Dancer.prototype.step.call(this);
+  this.$node.toggle();
+};
+
+BouncyDancer.prototype.setPosition = function(top, left){
+  // Use css top and left properties to position our <span> tag
+  // where it belongs on the page. See http://api.jquery.com/css/
+  //
+  var styleSettings = {
+    top: top,
+    left: left
+  };
+  this.$node.css(styleSettings);
+};
